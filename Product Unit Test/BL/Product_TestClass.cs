f0b0448bc6ac15362705f274186611f9c98a7d7f
Hà Vũ Sơn Tùng <Havusontung007@gmail.com>
2021-03-19 23:22:10 +0700

    Test Friday

--- Product Unit Test/BL/Product_TestClass.cs
+++ Product Unit Test/BL/Product_TestClass.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using SalesManagement.BL;
+
 
 namespace Product_Unit_Test.BL
 {
@@ -12,21 +15,10 @@ namespace Product_Unit_Test.BL
     class Product_TestClass
     {
         [TestCase("C0001", "Mouse", "Magic Mouse", "unit-1", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0002", "Monitor", "Magic Monitor", "unit-2", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0003", "Keyboard", "Magic Keyboard", "unit-3", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0004", "Webcam", "Magic Webcam", "unit-4", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0005", "HeadPhone", "Magic HeadPhone", "unit-5", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0006", "Monitor", "Magic Monitor", "unit-2", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0007", "Keyboard", "Magic Keyboard", "unit-3", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0008", "Webcam", "Magic Webcam", "unit-4", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0009", "HeadPhone", "Magic HeadPhone", "unit-5", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0010", "Mouse", "Magic Monitor", "unit-1", 100, 1000, true, "2021-03-2021")]
-
+        [TestCase(null, null, null, null, 100, 1000, true, "03-03-2021")]
+        [TestCase("", "", "", "", 100, 1000, true, "03-03-2021")]
         public void TC_01_Costructor_InitConstructor(string id, string name, string category, string unit, int price, int quantity, bool discontinued, string date)
         {
-
-            try
-            {
                 Product product = new Product(id, name, category, unit, price, quantity, discontinued, DateTime.Parse(date));
 
                 Assert.AreEqual(id, product.Id);
@@ -36,34 +28,16 @@ namespace Product_Unit_Test.BL
                 Assert.AreEqual(price, product.Price);
                 Assert.AreEqual(quantity, product.Quantity);
                 Assert.AreEqual(discontinued, product.Discontinued);
-                Assert.AreEqual(DateTime.Parse(date), product.Date);
-            }
-            catch (ArgumentNullException ae)
-            {
-                Assert.AreEqual("Parameter cannot be null or empty.", ae.Message);
-            }
-            catch (Exception e)
-            {
-                Assert.Pass(
-                     string.Format("Unexpected exception of type {0} caught: {1}",
-                                    e.GetType(), e.Message)
-                );
+                Assert.AreEqual(DateTime.Parse(date), product.Date);          
             }
 
 
-        }
+
         [TestCase("C0001", "Mouse", "Magic Mouse", "unit-1", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0002", "Monitor", "Magic Monitor", "unit-2", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0003", "Keyboard", "Magic Keyboard", "unit-3", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0004", "Webcam", "Magic Webcam", "unit-4", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0005", "HeadPhone", "Magic HeadPhone", "unit-5", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0006", "Monitor", "Magic Monitor", "unit-2", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0007", "Keyboard", "Magic Keyboard", "unit-3", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0008", "Webcam", "Magic Webcam", "unit-4", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0009", "HeadPhone", "Magic HeadPhone", "unit-5", 100, 1000, true, "03-03-2021")]
-        [TestCase("C0010", "Mouse", "Magic Monitor", "unit-1", 100, 1000, true, "03-03-2021")]
+        [TestCase(null, null, null, null, 100, 1000, true, "03-03-2021")]
+        [TestCase("", "", "", "", 100, 1000, true, "03-03-2021")]
 
-        public void TC_01_Costructor_Setter(string id, string name, string category, string unit, int price, int quantity, bool discontinued, string date)
+        public void TC_02_Costructor_Setter(string id, string name, string category, string unit, int price, int quantity, bool discontinued, string date)
         {
             Product product = new Product();
             product.Id = id;
@@ -86,9 +60,18 @@ namespace Product_Unit_Test.BL
             
 
         }
+        [TestCase]
+        public void TC_03_getAllProducts()
+        {
+            Assert.IsTrue(Product.GetAllProducts().Count >= 0);
+        }
+        [TestCase]
+        public void TC_04_TestConnectionString()
+        {
+            Assert.NotNull(ConfigurationManager.ConnectionStrings["ProductDB"]);
+        }
 
 
-        
 
 
     }
